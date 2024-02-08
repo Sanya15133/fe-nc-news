8bf3cd12e836f2129086a600151da983e8a38c3e
Sanya Hussain <sanz_133@hotmail.com>
2024-02-08 17:53:40 +0000

    task 7 done

--- api.js
+++ api.js
@@ -4,7 +4,6 @@ export const getArticles = () => {
   let endpoint = "https://nc-news-lu0p.onrender.com/api/articles";
 
   return axios.get(endpoint).then((response) => {
-    console.log(response.data.articles, "here");
     return response.data.articles;
   });
 };
@@ -13,7 +12,6 @@ export const getArticleById = (article_id) => {
   let endpoint = `https://nc-news-lu0p.onrender.com/api/articles/${article_id}`;
 
   return axios.get(endpoint).then((response) => {
-    console.log(response.data.article, "here");
     return response.data.article;
   });
 };
@@ -22,15 +20,15 @@ export const getCommentsById = (article_id) => {
   let endpoint = `https://nc-news-lu0p.onrender.com/api/articles/${article_id}/comments`;
 
   return axios.get(endpoint).then((response) => {
-    console.log(response.data.articles, "here");
     return response.data.articles;
   });
 };
 
 export const updateVotesById = (article_id, votes) => {
+
   return axios
     .patch(`https://nc-news-lu0p.onrender.com/api/articles/${article_id}`, {
-      votes: votes,
+      inc_votes: votes,
     })
     .then((response) => {
       return response.data.article;
